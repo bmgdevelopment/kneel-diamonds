897190f2db2be17ca13930868f1c2f33064d04f5
Brittany Garrett <brittanymgarrett@gmail.com>
2021-05-22 14:17:44 -0500

    Added another potential solution

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,24 @@ document.addEventListener('click', e => {
   }
 });
 
+// document.addEventListener('click', e => {
+//   if (e.target.id === 'orderButton') {
+
+//     const allRadioButtons = document.querySelectorAll('#radio_button');
+//     const btnArr = Array.from(allRadioButtons);
+
+//     let radioBtnArray = btnArr.map(checkedRadioButton => {
+//       if (checkedRadioButton.checked === true) {
+//         return checkedRadioButton;
+//       }
+//     });
+//     if (radioBtnArray.length === 3) {
+//       console.log('Order button has been pushed');
+//       addCustomOrder();
+//     }
+//   }
+// });
+
 //BELOW CODE LISTENS FOR STATECHANGED EVENT 
 //When it is dispatched by the database module, the main module will 
 //generate all the HTML again and display it.
@@ -41,11 +59,8 @@ document.addEventListener('stateChanged', () => {
 //   }
 // });
 
-// document.addEventListener('click', e => {
-//   if (e.target.id === 'radio_button') {
-//     if (e.target.id === 'orderButton') {
-//       console.log('Order button has been pushed');
-//       addCustomOrder();
-//     }
-//   }
-// });
+// above example cannot run due to the .getElementById reference all 'radio_button' instead of one
+// use id only when the item is unique and also add template literals to make it more specific
+//instead document.querySelectorAll('#radio_button') would then reference the input with the id radio_button
+//but moving forward, use class for more than one reference of similar items
+
